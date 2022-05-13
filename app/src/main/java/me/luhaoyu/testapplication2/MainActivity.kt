368c7dfea0887211ce14858747d2e2ed2c4a053e
luhaoyu <luhaoyu@kuaishou.com>
2022-05-13 17:13:43 +0800

    tmp commit

--- app/src/main/java/me/luhaoyu/testapplication2/MainActivity.kt
+++ app/src/main/java/me/luhaoyu/testapplication2/MainActivity.kt
@@ -7,5 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val kk = R.string.my_ccc
+        val navRouter = com.kwai.kim.router_android.R.id.nav_router
     }
 }
